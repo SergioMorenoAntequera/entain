b00549c43fa496d5826eb3c580cbcfdfea3d2e71
SergioMorenoAntequera <seranmoreno500@gmail.com>
2025-07-10 19:37:19 +0200

    feature:backend/disable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://frontend:5173', 'http://localhost:5173'],
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 
