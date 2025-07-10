70f89dc7f8c76cef84b798a791f4c7e5ed43f4e5
SergioMorenoAntequera <seranmoreno500@gmail.com>
2025-07-10 19:47:14 +0200

    feature:backend/adding remote for cors issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://34.241.185.65:5173'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
